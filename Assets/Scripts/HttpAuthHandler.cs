fe81af31889ca23207caace374778e0766128bbd
Juan José <67957125+JJaoMein@users.noreply.github.com>
2023-02-19 14:32:07 -0500

    Get Input Fields Data

--- Assets/Scripts/HttpAuthHandler.cs
+++ Assets/Scripts/HttpAuthHandler.cs
@@ -1,26 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.Rendering;
 using UnityEngine.UI;
 
 
 public class HttpAuthHandler : MonoBehaviour
 {
     [SerializeField] string serverApiURL;
+    [SerializeField] TMP_InputField userNameInput;
+    [SerializeField] TMP_InputField passwordInput;
 
 
     // Start is called before the first frame update
+
     void Start()
     {
 
     }
 
+
     public void SingUp()
     {
         User user = new User();
-        user.username = GameObject.Find("InputFieldUsername").GetComponent<InputField>().text;
-        user.password = GameObject.Find("InputFieldPassword").GetComponent<InputField>().text;
+        user.username = userNameInput.text;
+        user.password = passwordInput.text;
+        Debug.Log(user.username);
+        Debug.Log(user.password);
 
 
         string postData = JsonUtility.ToJson(user);
@@ -49,6 +57,13 @@ public class HttpAuthHandler : MonoBehaviour
             {
                 AuthJsonData jsonData = JsonUtility.FromJson<AuthJsonData>(www.downloadHandler.text);
                 Debug.Log(jsonData.usuario.username + "Se regitro con id: " + jsonData.usuario._id);
+            }
+            else
+            {
+                string mensaje = "Status: " + www.responseCode;
+                mensaje += "\ncontent-type:" + www.GetResponseHeader("content-type");
+                mensaje += "\nError: " + www.error;
+                Debug.Log(mensaje);
             }
         }
     }
